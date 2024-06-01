d3834fc2114a221f20505101112024a8315ecb78
JakeRArent <arentjake92@gmail.com>
2024-06-01 10:46:43 -0500

    Home.css, home.jsx, App.js updates

--- day-six/src/pages/App.js
+++ day-six/src/pages/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import SignIn from "./pages/SignIn";
+
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
+
+const router = createBrowserRouter([
+  /*{
+    path: "/home",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "pokemon/:pokemonID", element: <Pokemon /> },
+      { path: "wiki", element: <Wiki /> },
+    ]
+  },*/
+  {
+    path: "/",
+    element: <SignIn />
+  }
+]);
 
 function App() {
   return (
